ad180cb4d5c841a473d231c83969bd142b3f2e05
sababeg <sababegiashvili16@icloud.com>
2024-12-06 23:37:39 +0400

    goa

---
+++ level 20/homework/03.py
@@ -0,0 +1,7 @@
+age=input("ramdeni wlis xar? :")
+
+if  age < 12:
+ print("შენ არ ხარ 12 წლი")
+else:
+  print("შენ ხარ 12 წლი")
+
---
+++ level 22/homework/hw.py
@@ -0,0 +1,9 @@
+list_of_names=("lomi , kata , lomi , zebra, lomi")
+
+counter= 0
+name="lomi"
+for i in list_of_names:
+ 
+ if i in name:
+  counter = counter + 1
+  print (counter)
